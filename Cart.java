98ea015f14e526ed620ad4e1fe16bbc149078b44
lumiteeen <166654536+lumiteeen@users.noreply.github.com>
2024-04-17 16:32:01 -0400

    Update Cart.java

--- Cart.java
+++ Cart.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Cart {
     private int id;
@@ -46,3 +47,36 @@ public class Cart {
         this.discountedPrice = discountedPrice;
     }
 }
+
+public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.println("The MIST store:");
+            System.out.println("1. Browse Products");
+            System.out.println("2. Checkout");
+            System.out.println("3. Exit");
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("You chose Option A");
+                    // Perform action for Option A
+                    break;
+                case 2:
+                    System.out.println("You chose Option B");
+                    // Perform action for Option B
+                    break;
+                case 3:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please enter 1, 2, or 3.");
+            }
+
+        } while (choice != 3);
+
+        scanner.close();
+    }
